b0d3a275f0946ea33dbf8b33aa4d7bffdfc253cc
Jonathan Higgs <jonathan.higgs.11@mail.wbs.ac.uk>
2023-07-05 22:54:41 +0100

    Ready to build

--- README.md
+++ README.md
@@ -3,13 +3,17 @@
 Python event tracing using the [Trace Event Format](https://docs.google.com/document/d/1CvAClvFfyA5R-PhYUmn5OOQtYMH4h6I0nSsKchNAySU/edit)
 which is supported by Chromium browsers and other tools
 
+## Installing
+
+Install and update using [pip](https://pypi.org/project/pip/)
+
 ## Usage
 
 Add tracing to sections of your code and execute to generate a `json` trace file
 
 ```python
 import time
-from trace_events import profile, Profiler, EventTimer
+from trace_events import Profiler, profile, timeit
 
 @profile
 def foo():
@@ -20,12 +24,11 @@ def bar():
     time.sleep(1)
 
 def baz(value):
-    with EventTimer('baz::foo'):
+    with timeit('baz::foo'):
         foo()
 
-    with EventTimer('baz::bar', value=value):
+    with timeit('baz::bar', value=value):
         bar()
-
 
 if __name__ == '__main__':
     baz(10)
@@ -33,6 +36,26 @@ if __name__ == '__main__':
     Profiler.global_profiler().save_trace('trace.json')
 ```
 
+Traces are saved `atexit` to a default `trace.json` file, or can be configured with a call to `init_trace`
+
+```python
+import trace_events
+
+trace_events.init_trace(
+    trace_file_dir='./my-traces',
+    trace_file_name='my-trace.json',
+    # save_at_exit = True
+    # overwrite_trace_files=False,
+    # logger=logging.getLogger("trace_events")
+)
+```
+
 Open up a Chromium browser to [chrome://tracing](chrome://tracing) and load the file to view the trace
 
 ![Example image](media/example.png)
+
+More examples in the examples folder
+
+- [Basic Example](examples/basic-example.py)
+- [Logging Example](examples/logging-example.py)
+- [Tracing Disabled](examples/tracing-disabled.py)
--- setup.py
+++ setup.py
@@ -1,24 +1,55 @@
+import re
+from os import path
 import setuptools
 
-from trace_events import __author__ as author, __author_email__ as author_email, __version__ as version
 
+# Version Number
+package_name = 'trace_events'
+package_path = path.join('src', package_name, '__init__.py')
+with open(package_path, 'r') as file:
+    content = file.read()
+
+author, author_email, version = [
+    re.search(f"^__{v}__[\ \t]*=[\ \t]*'([^']+)'",
+              content, re.MULTILINE).group(1)
+    for v
+    in ['author', 'author_email', 'version']]
+
+# Readme
 with open('README.md', 'r') as file:
     long_description = file.read()
 
+
+# Run
 setuptools.setup(
-    name = 'trace_events',
-    version = version,
-    author = author,
-    author_email = author_email,
-    description = 'Python event tracing using the Trace Event Format supported by Chromium browsers',
-    long_description = long_description,
-    long_description_content_type = 'text/markdown',
-    url = 'https://github.com/JonathanHiggs/trace_events',
-    packages = setuptools.find_packages(),
-    classifiers = [
-        'Programming Language :: Python :: 3',
+    name='trace-events',
+    version=version,
+    author=author,
+    author_email=author_email,
+    description='Python event tracing using the Trace Event Format supported by Chromium browsers',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    url='https://github.com/JonathanHiggs/trace_events',
+    license='MIT',
+    project_urls={
+        'Source': 'https://github.com/JonathanHiggs/trace_events',
+        'Bug Tracker': 'https://github.com/JonathanHiggs/trace_events/issues'
+    },
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
         'License :: OSI Approved :: MIT License',
-        'Operating System :: OS Independent'
+        'Natural Language :: English',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
+        'Programming Language :: Python :: 3.11',
+        'Programming Language :: Python :: 3.12',
+        'Topic :: Software Development :: Debuggers'
     ],
-    python_requires = '>=3.9'
-)
+    keywords='trace tracing event profile profiler',
+    package_dir={"": "src"},
+    packages=setuptools.find_packages(where='src'),
+    python_requires='>=3.8')
